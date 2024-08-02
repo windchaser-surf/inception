da7cbc904f21d361a4fdd33a7cd806a9935550fe
windchaser-surf <wechslerf@gmail.com>
2024-08-02 18:14:26 +0200

    FEATURE: added implemented redis to wp container

--- srcs/requirements/wordpress/tools/init.sh
+++ srcs/requirements/wordpress/tools/init.sh
@@ -6,9 +6,16 @@ wp config set DB_NAME $DB_NAME --allow-root
 wp config set DB_USER $DB_USER --allow-root
 wp config set DB_PASSWORD $DB_PW --allow-root
 wp config set DB_HOST $DB_HOST --allow-root
-
+wp config set WP_CACHE true --allow-root
 wp core install --url=fwechsle.42.fr --title=$WP_TITLE --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PW --admin_email=$WP_ADMIN_EMAIL --allow-root
 wp user create $WP_USER $WP_EMAIL --user_pass=$WP_PW --role=$WP_ROLE --allow-root
-wp --info 
+wp config set WP_REDIS_HOST redis --allow-root
+wp config set WP_REDIS_PORT 6379 --allow-root
+
+wp plugin install redis-cache --activate --allow-root
+
+#wp --info 
+
+wp redis enable --allow-root
 
 php-fpm8.2 -F
